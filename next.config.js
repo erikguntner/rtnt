ec70024f658ff715757fbc7e02ad567b81c95bd8
Erik <eguntner2291@gmail.com>
2020-01-31 01:07:31 -0800

    :wrench: cleaned up next.config file
    removed unnecessary old configuration comments

--- next.config.js
+++ next.config.js
@@ -1,13 +1,3 @@
-// const withCss = require('@zeit/next-css');
-// require('dotenv').config();
-
-// module.exports = withCss({
-//   env: {
-//     GRAPH_HOPPER_KEY: process.env.GRAPH_HOPPER_KEY,
-//     MAPBOX_TOKEN: process.env.MAPBOX_TOKEN,
-//   },
-// });
-
 require('dotenv').config();
 
 module.exports = {
