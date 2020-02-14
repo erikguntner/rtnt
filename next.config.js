ccca5e0eb9caad79e00fb50deaec6a887eca947c
Erik <eguntner2291@gmail.com>
2020-02-13 16:34:11 -0800

    added env vars to next.config.js

--- next.config.js
+++ next.config.js
@@ -4,5 +4,7 @@ module.exports = {
   env: {
     GRAPH_HOPPER_KEY: process.env.GRAPH_HOPPER_KEY,
     MAPBOX_TOKEN: process.env.MAPBOX_TOKEN,
+    JWT_SECRET: process.env.JWT_SECRET,
+    MONGO_URI: process.env.MONGO_URI,
   },
 };
