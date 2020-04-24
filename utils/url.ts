149e14ce9f10fa7376c7590413c45a506a95986a
Erik <eguntner2291@gmail.com>
2020-04-24 14:06:55 -0700

    trying out NOW_URL env

--- utils/url.ts
+++ utils/url.ts
@@ -3,6 +3,8 @@
 //     ? 'https://rtnt.now.sh'
 //     : 'http://localhost:3000';
 
+console.log(process.env.NOW_URL);
+
 const API_URL =
   process.env.NODE_ENV === 'production'
     ? process.env.NOW_URL
