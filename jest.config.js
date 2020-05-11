db0feb8a21d52809c4f8396a5f6d6a1f4b95ac5f
Erik <eguntner2291@gmail.com>
2020-05-11 12:38:08 -0700

    gpx file download working!, upgraded to latest nextjs release

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  collectCoverageFrom: [
+    '**/*.{js,jsx,ts,tsx}',
+    '!**/*.d.ts',
+    '!**/node_modules/**',
+  ],
+  setupFilesAfterEnv: [
+    '@testing-library/jest-dom/extend-expect',
+    'jest-styled-components',
+  ],
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': 'ts-jest',
+  },
+  transformIgnorePatterns: ['/node_modules/'],
+};
--- utils/createGPXFile.ts
+++ utils/createGPXFile.ts
@@ -1,41 +1,25 @@
 import { calculateDistance, abbreviatedDistance } from './calculateDistance';
 
-export const formatGPXFile = (lines: number[][][], distance: number[], units: 'miles' | 'kilometers'): string => {
-  const distanceInUnits = calculateDistance(lines, units, 2);
-  const unitsAbbrev = abbreviatedDistance(units);
-  const points = lines.reduce((accum, curr) => accum.concat(curr));
-  const xml = `
-  <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-  <gpx version="1.1" creator="https://rtnt.now.sh" xmlns="http://www.topografix.com/GPX/1/1">
-    <metadata>
-      <name>${distanceInUnits} ${unitsAbbrev} route</name>
-      <link href="https://rtnt.now.sh">
-        <text>On The Go Map</text>
-      </link>
-      <time>${new Date()}</time>
-      <copyright author="Run Tracker">
-        <year>2020</year>
-      </copyright>
-    </metadata>
-    <rte>
-      <name>${distanceInUnits} ${unitsAbbrev} route</name>
-      ${points.map((point) => `<rtept lat="${point[1]}" lon="${point[0]}"/>`).join('\n')}
-    </rte>
-  </gpx>`;
+const createXmlString = (lines: number[][][]): string => {
+  let result = '<gpx xmlns="http://www.topografix.com/GPX/1/1" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd" version="1.1" creator="runtracker"><metadata/><trk><name></name><desc></desc>'
+  result += lines.reduce((accum, curr) => {
+    let segmentTag = '<trkseg>';
+    segmentTag += curr.map((point) => `<trkpt lat="${point[1]}" lon="${point[0]}"><ele>${point[2]}</ele></trkpt>`).join('');
+    segmentTag += '</trkseg>'
 
-  return xml;
+    return accum += segmentTag;
+  }, '');
+  result += '</trk></gpx>';
+  return result;
 }
-
 
 export const downloadGPXFile = (
   lines: number[][][],
   distance: number[],
   units: 'miles' | 'kilometers'
 ) => {
-  const data = formatGPXFile(lines, distance, units);
-  const blob = new Blob([data], { type: 'text/plain' });
-  const url = window.URL.createObjectURL(blob);
-
+  const xml = createXmlString(lines);
+  const url = 'data:text/json;charset=utf-8,' + xml;
   const link = document.createElement('a');
   link.download = `${distance[distance.length - 1]}-${units}.gpx`;
   link.href = url;
