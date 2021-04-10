84af392322077e7295be0824970ae267c809dcde
Erik <eguntner2291@gmail.com>
2021-04-10 14:41:08 -0700

    Update env varoables

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -1,5 +1,9 @@
+console.log('node environment', process.env.NODE_ENV);
+console.log('vercel environment', process.env.VERCEL_ENV);
+
+
 const API_URL =
-  process.env.NODE_ENV === 'production' || process.env.VERCEL === 'preview'
+  process.env.NODE_ENV === 'production' || process.env.VERCEL_ENV === 'preview'
     ? process.env.VERCEL_URL
     : 'http://localhost:3000';
 
